b7887bfef2638002f201e2ba699630eeac7e85c5
Brandon Ward <brandonward.gk@gmail.com>
2021-02-26 15:45:29 -0700

    Add planning, converted App into class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
-}
+import React, { Component } from 'react'
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+       <div className="App">
+         <h1>Hello</h1>
+       </div>
+    )
+  }
+};
