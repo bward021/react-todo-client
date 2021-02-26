10e2742bf3bf1b309faf0d0d50a1f67967db11af
Brandon Ward <brandonward.gk@gmail.com>
2021-02-26 15:58:30 -0700

    added form to app

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Todo React Client
+
+# Components: 
+
+- Todo-Form
+  - form
+    - input
+    - button
+
+- Todo-Item:
+  - checkbox
+  - item name
+  - delete button
+
+- delete-all-button:
+  - button to delete all items
+
+- State:
+  - title -> string
+  - done -> boolean
+  - todos -> array
--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export default class App extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({
+      title: ""
+    })
+  }
+
   render() {
     return (
-       <div className="App">
-         <h1>Hello</h1>
-       </div>
-    )
+      <div className="app">
+        <h1>Todo List</h1>
+        <form className="add-todo">
+          <input
+            type="text"
+            placeholder="Add Todo"
+            onChange={this.handleChange}
+            value={this.state.title}
+          />
+          <button type="submit" onSubmit={this.handleSubmit}>Add</button>
+        </form>
+      </div>
+    );
   }
-};
+}
+
+export default App;
